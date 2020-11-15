11e3c5c92f70484f4e5def7bc0e3f53528285c5b
rajithabhanuka <rajithabhanuka1@gmail.com>
2020-11-15 14:25:56 +0530

    h2 configured new

--- src/main/java/com/test/db/MyDbConfig.java
+++ src/main/java/com/test/db/MyDbConfig.java
@@ -2,7 +2,6 @@ package com.test.db;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
